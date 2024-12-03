ba5c0fc31425029ad093965c7ba4897964c886cf
monchitos822 <monchitos822@gmail.com>
2024-12-03 14:01:42 +0100

    v3.4 Changes in some classes

--- FileSystem/src/structures/Cluster.java
+++ FileSystem/src/structures/Cluster.java
@@ -2,13 +2,8 @@ package structures;
 
 public abstract class Cluster {
 	String name;
-	int spaceNeeded;
-	int firstCluster;
 	
 	public Cluster(String name, int spaceNeeded) {
-		this.name = name;
-		this.spaceNeeded = spaceNeeded;
-		
 		
 	}
 
